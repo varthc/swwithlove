dd9d104a275ea078acb31b9ef82862052c8d4f80
C / <32149970+varthc@users.noreply.github.com>
2020-03-22 22:10:09 -0300

    Hide php errors.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 /*var_dump(openssl_get_cert_locations());
 echo "openssl.cafile: ", ini_get('openssl.cafile'), "\n";
 echo "curl.cainfo: ", ini_get('curl.cainfo'), "\n";*/
